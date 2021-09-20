7d26bd3269d4e890daf8abf07a04fe86cc6ade37
nejla <nejlalowe@gmail.com>
2021-09-20 12:38:51 +0100

    second min requirements

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,11 @@
 def fizzbuzz(number)
-  "fizz"
+  if number == 3
+    return "fizz"
+  elsif number == 5
+    return "buzz"
+  elsif number == 15
+    return "fizzbuzz"
+  end
 end
+
+p fizzbuzz(10)
