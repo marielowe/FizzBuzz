caaee04cf19f4d6191c939ee248fd5d760d3a5af
Lauren Ashton <l.v.ashton@outlook.com>
2021-09-20 13:09:02 +0100

    Completed FizzBuzz sequence test

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,11 +1,13 @@
 def fizzbuzz(number)
-  if number == 3
-    return "fizz"
-  elsif number == 5
-    return "buzz"
-  elsif number == 15
+  if number % 15 == 0
     return "fizzbuzz"
+  elsif number % 3 == 0
+    return "fizz"
+  elsif number % 5 == 0
+    return "buzz"
+  else
+    return number
   end
 end
 
-p fizzbuzz(10)
+p fizzbuzz(4)
